357d3876a31ecb4eea28114f2750b4c6e3487535
AlexeyBulaev <bulaev.a.u@gmail.com>
2019-07-07 22:46:41 +0300

    Adding base Entity

--- pet-clinic-data/src/main/java/edu/abulaev/petclinic/model/Person.java
+++ pet-clinic-data/src/main/java/edu/abulaev/petclinic/model/Person.java
@@ -1,7 +1,7 @@
 package edu.abulaev.petclinic.model;
 
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastName;
 
--- pet-clinic-data/src/main/java/edu/abulaev/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/edu/abulaev/petclinic/model/PetType.java
@@ -1,6 +1,6 @@
 package edu.abulaev.petclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity{
 
     private String type;
 
