651685096fb28a06e2554acbc35f6e04f8e45b49
AlexeyBulaev <bulaev.a.u@gmail.com>
2019-07-10 01:17:01 +0300

    Implementing Map Based Services

---
+++ pet-clinic-data/src/main/java/edu/abulaev/petclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package edu.abulaev.petclinic.services.map;
+
+import edu.abulaev.petclinic.model.Vet;
+import edu.abulaev.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long>
+        implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
