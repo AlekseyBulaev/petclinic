6466be6b12dfce380ed8bbcb57ec4c7e0031f3cb
AlexeyBulaev <bulaev.a.u@gmail.com>
2019-07-13 13:53:15 +0300

    Create Pet Type, Pet, Visit Entities. Closes #2

--- pet-clinic-data/src/main/java/edu/abulaev/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/edu/abulaev/petclinic/model/Owner.java
@@ -1,5 +1,17 @@
 package edu.abulaev.petclinic.model;
 
+import java.util.Set;
+
 public class Owner extends Person {
 
+
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
