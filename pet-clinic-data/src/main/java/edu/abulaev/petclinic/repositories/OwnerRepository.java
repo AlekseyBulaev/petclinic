6338fb80b897de54ccffbbcbd398a11cba837a0c
AlexeyBulaev <bulaev.a.u@gmail.com>
2019-07-21 10:30:35 +0300

    added spring data jpa for owners

--- pet-clinic-data/src/main/java/edu/abulaev/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/edu/abulaev/petclinic/repositories/OwnerRepository.java
@@ -3,6 +3,7 @@ package edu.abulaev.petclinic.repositories;
 import edu.abulaev.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-public interface OwnerREpository extends CrudRepository<Owner, Long> {
+public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
+    Owner findByLastName(String lastName);
 }
